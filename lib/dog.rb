e504340ebbbab2a0a4b83df684d30d74aa9a30b6
J K <jkim65@buffalo.edu>
2020-10-11 03:30:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,6 +11,11 @@ class Dog
 
   def self.create_table
     sql = <<-SQL
-    SQL 
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    )
+    SQL
   end
 end
