efc6be3411e54f2213783080a99b8995bcf2b7f1
J K <jkim65@buffalo.edu>
2020-10-11 03:53:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,6 +46,7 @@ class Dog
     SQL
     DB[:conn].execute(sql, self.name, self.breed)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-    
+    new_dog = self
+    new_dog
   end
 end
